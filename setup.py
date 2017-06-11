a7eecc274234c7edc4fc53550e07d97e2a34beb9
Sijis Aviles <sijis.aviles+github@gmail.com>
2017-06-11 00:22:04 -0500

    fix: Remove ez_setup

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
-#! /usr/bin/env python
-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 
 setup(
